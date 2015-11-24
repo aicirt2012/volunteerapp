ad1a3a0f46ad58420569eb5fed1ceab55edcf86b
Felix Michel <felix.michel@tum.de>
2015-11-24 12:39:16 +0100

    jser

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    'apiurl': "http://vmmatthes21.informatik.tu-muenchen.de/api/0.1",
+    'sc' : {
+        'url': "http://vmmatthes21.informatik.tu-muenchen.de/api/0.1",
+        'pass' : 'sebis',
+        'user' : 'sociocortex.sebis@tum.de'
+    },
     'email':{
         'user': 'muc.refugees',
         'pass': 'FD37hZJL8z3eyBUbBcho'
--- server/routes/user/user.js
+++ server/routes/user/user.js
@@ -5,19 +5,23 @@ var mailer = require('../../util/mailer');
 var request = require('request');
 var config = require('../../../config')
 
+/** list all users */
 router.get('/list', function(req, res, next) {
     res.json(JSON.parse(fs.readFileSync('server/routes/user/user.list.json')));
 });
 
+/** returns session user */
 router.get('/me', function(req, res, next) {
     console.log('me user');
     res.send();
 });
 
+/** update user */
 router.put('/', function(req, res, next) {
     console.log('update user');
     res.send();
 });
+
 
 router.get('/test2', function(req, res, next) {
     mailer.send({
@@ -39,23 +43,36 @@ router.get('/test3', function(req, res, next) {
     request(config.apiurl+'/users', function (error, response, body) {
         if (!error && response.statusCode == 200) {
             console.log(body);
-            //console.log(response);
+            res.send(JSON.parse(body));
+        }else{
+            res.send('err');
+        }
+    });
+});
+
+/** create new user*/
+router.get('/test4', function(req, res, next) {
+
+
+    var auth = new Buffer(config.sc.user + ':' + config.sc.pass).toString('base64');
+    var req = {
+        url: 'http://vmmatthes21.informatik.tu-muenchen.de/api/0.1/users',
+        headers: {
+            'Authorization': 'Basic ' + auth,
+            'Content-Type': 'application/json'
+        }
+    };
+     console.log('request prepared', JSON.stringify(req));
+    request(req, function (error, response, body) {
+        console.log(error);
+        if (!error && response.statusCode == 200) {
+            console.log(body);
             res.send(JSON.parse(body));
         }else{
             res.send('err');
         }
     });
 
-    /*
-    request('http://localhost:3000/api/user/list', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body);
-            res.send(body);
-        }else{
-            res.send('err');
-        }
-
-    })*/
 });
 
 module.exports = router;

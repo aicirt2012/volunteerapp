2ed5d651b9b8df4e362e81a41ebc4fc0de229fe2
Felix Michel <felix.michel@tum.de>
2015-12-02 13:07:43 +0100

    user

--- client/views/user/user.js
+++ client/views/user/user.js
@@ -1,11 +1,23 @@
-app.controller('UserCtrl', ['$http', '$scope', function($http, $scope) {
+app.controller('UserCtrl', ['$http', '$scope', '$location', function($http, $scope, $location) {
+
+
 
     var me = $scope;
+    me.selectedRoleTab = 1;
 
-    me.selectRole = function(role){
-        console.log('help');
-        window.location.href = '#/user/'+role;
-    };
+
+    me.$watch('selectedRoleTab', function(newValue, oldValue) {
+        var role = null;
+        switch (newValue){
+            case 0: role = 'helper'; break;
+            case 1: role = 'team'; break;
+            case 2: role = 'org'; break;
+        }
+        console.log('Role: '+role);
+      // $location.path("/user/org", false);
+    });
+
+
 
 
 

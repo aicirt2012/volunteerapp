3626c5c46e72bb81289f4148aa741cca2cd97339
Felix Michel <felix.michel@tum.de>
2015-12-07 16:40:05 +0100

    index clean up

--- client/views/login/login.js
+++ client/views/login/login.js
@@ -1,9 +1,10 @@
-app.controller('LoginCtrl', ['$scope', 'User', '$routeParams', '$mdSidenav', function($scope, User, $routeParams, $mdSidenav) {
+app.controller('LoginCtrl', ['$scope', 'User', '$mdSidenav', function($scope, User, $mdSidenav) {
 
     console.log($mdSidenav('left'));
     $mdSidenav('left').toggle();
     var me = $scope;
     console.log('login');
+    
 
     me.login = function(){
         console.log(me.email+" "+me.pw);

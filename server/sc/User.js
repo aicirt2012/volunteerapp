14ad9014c77f3ceef4652923ed3976d69ee9b772
Felix Michel <felix.michel@tum.de>
2015-12-10 18:33:58 +0100

    json

--- server/sc/User.js
+++ server/sc/User.js
@@ -13,10 +13,8 @@ var User = EntityType.define({
     pw: {type:'Text'},
     notes: {type:'Text'},
     role: {type:'Text'},
-    availability: {type: 'json'}
-    //  'Structure<mo:Structure<morning:Boolean,afternoon:Boolean,evening:Boolean>, tu:Structure<morning:Boolean,afternoon:Boolean,evening:Boolean>, we:Structure<morning:Boolean,afternoon:Boolean,evening:Boolean>,  th:Structure<morning:Boolean,afternoon:Boolean,evening:Boolean>, fr:Structure<morning:Boolean,afternoon:Boolean,evening:Boolean>, sa:Structure<morning:Boolean,afternoon:Boolean,evening:Boolean>, su:Structure<morning:Boolean,afternoon:Boolean,evening:Boolean>, >'}},
-   // availability: {type: 'json', options: {jsonTypeDefinition:   'Structure<mo:Structure<morning:Boolean>'}},
-   // test: {type:'json', options: {jsonTypeDefinition:   'Structure<street:String,city:String>'}   }
+    availability: {type: 'json', options: {jsonTypeDefinition: 'Structure<mo:Structure<morning:Boolean,afternoon:Boolean,evening:Boolean>, tu:Structure<morning:Boolean,afternoon:Boolean,evening:Boolean>, we:Structure<morning:Boolean,afternoon:Boolean,evening:Boolean>,  th:Structure<morning:Boolean,afternoon:Boolean,evening:Boolean>, fr:Structure<morning:Boolean,afternoon:Boolean,evening:Boolean>, sa:Structure<morning:Boolean,afternoon:Boolean,evening:Boolean>, su:Structure<morning:Boolean,afternoon:Boolean,evening:Boolean> >'}}
+
 }, 'user');
 
 User.genders = {

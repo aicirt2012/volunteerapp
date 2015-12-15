e19e48b5988b1206123748fcb26b88e8bc3efd1f
Felix Michel <felix.michel@tum.de>
2015-12-15 13:30:56 +0100

    EventHelper with type user

--- server/sc/EventHelper.js
+++ server/sc/EventHelper.js
@@ -6,7 +6,7 @@ var EntityType = require('../sc/EntityType');
 var EventHelper = EntityType.define({
     id: {type:'String'},
     registered: {type:'String'},
-    helper: {type:'String'}
+    helper: {type: 'link', options:{entityType: {id:'user'}} }
 }, 'eventhelper');
 
 

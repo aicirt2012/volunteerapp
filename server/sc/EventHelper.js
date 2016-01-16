6a482c329a433477b148e60a9057760bb10427e8
Felix Michel <felix.michel@tum.de>
2016-01-16 01:24:27 +0100

    EventHelper Model updated

--- server/sc/EventHelper.js
+++ server/sc/EventHelper.js
@@ -6,7 +6,8 @@ var EntityType = require('../sc/EntityType');
 var EventHelper = EntityType.define({
     id: {type:'String'},
     registered: {type:'String'},
-    helper: {type: 'link', options:{entityType: {id:'user'}} }
+    helper: {type: 'link', options:{entityType: {id:'user'}} },
+    event: {type: 'link', options:{entityType: {id:'event'}} }
 }, 'eventhelper');
 
 

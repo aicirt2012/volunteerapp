ef9585456c119af14c00d520afc7600355c0cc4e
Felix Michel <felix.michel@tum.de>
2016-02-25 23:20:38 +0100

    log level

--- server/sc/Log.js
+++ server/sc/Log.js
@@ -8,10 +8,34 @@ var Log = EntityType.define({
     userid: {type: 'String'},
     userrole:  {type: 'String'},
     date: {type: 'Date'},
+    level: {type: 'String'},
     action: {type:'String'},
     description: {type:'String'}
 }, 'log');
 
+Log.level = {
+    INFO: 'INFO',
+    ERR: 'ERR'
+}
+
+Log.actions = {
+    LOGIN: 'LOGIN',
+    LOGIN_FAILED: 'LOGIN_FAILED',
+    LOGOUT: 'LOGOUT'
+}
+
+Log.log = function(user, level, action, description){
+    Log.save({
+        username: user.name,
+        useremail: user.email,
+        userid: user.id,
+        userrole:  user.role,
+        date: new Date().toISOString(),
+        level: level,
+        action: action,
+        description: description
+    });
+}
 
 module.exports = Log;
 

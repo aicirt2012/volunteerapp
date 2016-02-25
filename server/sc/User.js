84e5e8a7c47cc5da78e63c04126c8a23b8f81623
Felix Michel <felix.michel@tum.de>
2016-02-25 18:37:12 +0100

    fix

--- server/sc/User.js
+++ server/sc/User.js
@@ -71,13 +71,13 @@ User.findAvailableUsers = function(start, end, cb){
     function dayOfWeekLabel(date){
         var dayOfWeek = parseInt(moment(date).format('e'));
         switch(dayOfWeek){
-            case(1): return 'mo';
-            case(2): return 'tu';
-            case(3): return 'we';
-            case(4): return 'th';
-            case(5): return 'fr';
-            case(6): return 'sa';
-            case(7): return 'su';
+            case(0): return 'mo';
+            case(1): return 'tu';
+            case(2): return 'we';
+            case(3): return 'th';
+            case(4): return 'fr';
+            case(5): return 'sa';
+            case(6): return 'su';
         }
     }
 }

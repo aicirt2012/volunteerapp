5f6682d598a878e4435bb674b9392e14abcda3cb
Felix Michel <felix.michel@tum.de>
2016-02-28 16:41:07 +0100

    mydata photo upload fixes

--- client/views/mydata/mydata.js
+++ client/views/mydata/mydata.js
@@ -35,7 +35,6 @@ app.controller('MyDataCtrl', ['$scope', '$mdSidenav', 'user', 'User', 'MyData', 
     }
 
     me.submitAvailability = function(){
-
         MyData.availability.update(me.user.availability);
     }
 
@@ -44,22 +43,6 @@ app.controller('MyDataCtrl', ['$scope', '$mdSidenav', 'user', 'User', 'MyData', 
         MyData.photo.save({picture:dataUrl}, function(){
             console.log('finsh');
         });
-        /*
-         Upload.upload({
-         url: 'http://localhost:3000/api/mydata/photo',
-         data: {
-         file: Upload.dataUrltoBlob(dataUrl)
-         },
-         }).then(function (response) {
-         $timeout(function () {
-         $scope.result = response.data;
-         });
-         }, function (response) {
-         if (response.status > 0) $scope.errorMsg = response.status
-         + ': ' + response.data;
-         }, function (evt) {
-         $scope.progress = parseInt(100.0 * evt.loaded / evt.total);
-         });*/
     }
 
     $mdSidenav('left').open();

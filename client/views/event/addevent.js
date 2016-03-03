9c94271eaf4a5fc1394e1d9b6bae2dd299cb18b6
Niklas Scholz <niklas.scholz@tum.de>
2016-03-03 12:53:13 +0100

    removed unused functions in addevent

--- client/views/event/addevent.js
+++ client/views/event/addevent.js
@@ -25,36 +25,6 @@ app.controller('AddEventCtrl', ['$scope', '$mdSidenav', 'Event', '$mdpDatePicker
         window.location.href = '#/eventcalendar';
     }
 
-    me.saveStartDate = function(date){
-        if(date) {
-            me.event.startdate.setDate(date.getDate());
-            me.event.startdate.setMonth(date.getMonth());
-            me.event.startdate.setFullYear(date.getFullYear());
-        }
-    }
-
-    me.saveStartTime = function(time){
-        if(time) {
-            me.event.startdate.setHours(time.getHours());
-            me.event.startdate.setMinutes(time.getMinutes());
-        }
-    }
-
-    me.saveEndDate = function(date){
-        if(date) {
-            me.event.enddate.setDate(date.getDate());
-            me.event.enddate.setMonth(date.getMonth());
-            me.event.enddate.setFullYear(date.getFullYear());
-        }
-    }
-
-    me.saveEndTime = function(time){
-        if(time) {
-            me.event.enddate.setHours(time.getHours());
-            me.event.enddate.setMinutes(time.getMinutes());
-        }
-    }
-
     me.pickStartTime = function(ev) {
         $mdpTimePicker(ev, me.event.startdate).then(function(time) {
             time.setSeconds(0);

fff7f5914801875caccb0c9a918ee11efb74c904
Felix Michel <felix.michel@tum.de>
2016-03-08 14:02:20 +0100

    config fix

--- config.js
+++ config.js
@@ -6,18 +6,14 @@ module.exports = {
         secret: '382a4b7a5745454f3b44346d27744b2d305b3b58394f4d75375e7d7670',
         expiresInSeconds: 86400
     },
-    'sc' : {
-        'url': 'http://localhost:8083/intern/tricia/api/v1',
-        'pass' : 'ottto',
-        'user' : 'mustermann@test.sc',
-        'workspaceId' : 'refugeeapp'
+    sc : {
+        url: 'http://localhost:8083/intern/tricia/api/v1',
+        pass : 'ottto',
+        user : 'mustermann@test.sc',
+        workspaceId : 'refugeeapp'
     },
-    'email':{
-        'user': 'muc.refugees',
-        'pass': 'FD37hZJL8z3eyBUbBcho'
+    email:{
+        user: 'muc.refugees',
+        pass: 'FD37hZJL8z3eyBUbBcho'
     }
 };
-
-//"http://vmmatthes21.informatik.tu-muenchen.de/api/v1"
-//'sebis',
-//'sociocortex.sebis@tum.de'

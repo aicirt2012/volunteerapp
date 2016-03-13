b7316c5f4c544093714ac0c742fef6e796b6ad54
Felix Michel <felix.michel@tum.de>
2016-03-13 18:21:54 +0100

    prefilled login data removed

--- client/views/login/login.js
+++ client/views/login/login.js
@@ -1,21 +1,15 @@
 app.controller('LoginCtrl', ['$scope', 'User', '$mdSidenav', '$mdDialog', function($scope, User, $mdSidenav, $mdDialog) {
 
     var me = $scope;
-
-    //TODO Remove only for simple testing
-    me.email = 'user2@tum.de';
-    me.pw = '123';
     me.inAction = false;
 
     me.login = function(email, pw){
         me.inAction = true;
         User.login(email, pw).then(function(data){
-            //me.inAction = false;
             $scope.$parent.initMenu();
             window.location.href = '#/eventcalendar';
         }, function(){
             me.inAction = false;
-            console.log('login failed');
         });
     }
 

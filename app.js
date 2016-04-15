3c0b63877299fd2b64ae1ffbca77bd91c9b92613
Michael Ostner <michael.ostner@tum.de>
2016-04-15 12:26:16 +0200

    typo

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.use(function (req, res, next) {
 // partially adapted from https://github.com/brianc/node-domain-middleware
 app.use(function (err, req, res, next) {
     var body;
-    if (app.get('env') == 'development') {
+    if (app.get('env') === 'development') {
         // development error handler
         // will print stacktrace
         body = {

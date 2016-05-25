0e26240ba55c2dd55812f5ac50b73d9845755e4c
Felix Michel <felix.michel@tum.de>
2016-05-26 00:00:31 +0200

    user add lib

--- server/model/mo/User.js
+++ server/model/mo/User.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bcrypt = require("bcrypt-nodejs");
+var generator = require('generate-password');
 
 var Roles = {
     HELPER: 'HELPER',

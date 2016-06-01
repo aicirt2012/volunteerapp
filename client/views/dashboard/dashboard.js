3278c9f892191403f31d6659d3cd4cb39dda62ce
Felix Michel <felix.michel@tum.de>
2016-06-01 23:44:39 +0200

    fix

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -1,4 +1,4 @@
-app.controller('DashboardCtrl', ['$scope', '$mdSidenav', 'overview', function($scope, $mdSidenav, overview) {
+app.controller('DashboardCtrl', ['$scope', '$mdSidenav', 'overview', 'Dashboard', function($scope, $mdSidenav, overview, Dashboard) {
 
 
     var me = $scope;
@@ -8,6 +8,16 @@ app.controller('DashboardCtrl', ['$scope', '$mdSidenav', 'overview', function($s
     me.breadcrumb = function(){
         return 'Dashboard';
     };
+
+    $scope.$watch('selectedTabNr', function(current, old){
+       if(current != old && current==1){
+           console.log('load data');
+           Dashboard.logs(function(logs) {
+               console.log('finish loading',JSON.parse(JSON.stringify(logs)));
+               me.logs = JSON.parse(JSON.stringify(logs));
+           });
+       }
+    });
 
     $mdSidenav('left').open();
 

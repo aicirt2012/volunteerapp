06f7ee63fdaf0b0c6aa3e331085c82f23963f366
Niklas Scholz <niklas.scholz@tum.de>
2016-06-01 16:23:42 +0200

    Added search functionality for organisations

--- client/views/organization/organizationlist.js
+++ client/views/organization/organizationlist.js
@@ -4,6 +4,7 @@ app.controller('OrganizationListCtrl', ['$scope', '$mdSidenav', 'organizations',
     var me = $scope;
     me.addVisible = User.isOrganizer() || User.isAdmin();
     me.organizationlist = organizations;
+    me.searchText = "";
 
     me.selectOrganization = function(id){
         window.location.href = '#/organization/'+id;
@@ -11,6 +12,28 @@ app.controller('OrganizationListCtrl', ['$scope', '$mdSidenav', 'organizations',
 
     me.addOrganization = function(){
         window.location.href = '#/addorganization';
+    }
+
+    me.filterOrganization = function(organization){
+        var nameBool = true;
+        var telBool = true;
+        var emailBool = true;
+        if(me.searchText != "") {
+            nameBool = organization.name.toLowerCase().indexOf(me.searchText.toLowerCase()) > -1;
+            emailBool = organization.email.indexOf(me.searchText.toLowerCase()) > -1;
+            if(organization.tel)
+                telBool = organization.tel.indexOf(me.searchText) > -1;
+            else
+                telBool = false;
+        }
+        return (nameBool || emailBool || telBool);
+    }
+
+    me.highlight = function(search, text){
+        if (search)
+            return text.replace(new RegExp('('+search+')', 'gi'),'<b>$1</b>');
+        else
+            return text;
     }
 
     me.breadcrumb = 'Einrichtungsverwaltung';

d1a86a6d46afab363e4a5eb8bf816d6c776bec02
Felix Michel <felix.michel@tum.de>
2016-06-02 18:31:23 +0200

    fixes

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -17,61 +17,67 @@ app.controller('DashboardCtrl', ['$scope', '$mdSidenav', 'overview', 'Dashboard'
        }
     });
 
-    Dashboard.eventReport(function(events) {
+    me.createReport = function() {
+        Dashboard.eventReport(function (events) {
 
-        var doc = {
-            content: [
-                {text: 'This is an sample PDF printed with pdfMake', style: 'header'}
-            ],
-            footer: [
-                {text: 'VolunteerApp | www.volunteers.in.tum.de | '+moment().format('MMMM Do YYYY, h:mm:ss a'), style: 'footer'}
-            ],
-            styles: {
-                header: {
-                    fontSize: 12,
-                    color: '#0277BD',
-                    margin: [0,5,0,2]
-                },
-                headline: {
-                    fontSize: 12,
-                    color: '#0277BD',
-                    margin: [0,5,0,2]
-                },
-                footer: {
-                    fontSize: 10,
-                    color: 'gray',
-                    margin: [45,0,0,0]
-                },
-                content: {
-                    fontSize: 10,
-                    color: 'black',
-                    margin: [0,0,0,2]
-                },
-                separator: {
-                    margin: [0,0,0,8]
+            var doc = {
+                content: [
+                    {text: 'VolunteerApp Event Report', style: 'header'}
+                ],
+                footer: [
+                    {
+                        text: 'VolunteerApp  |  www.volunteers.in.tum.de  |  Dokument erstellt am: ' + moment().format('DD.MM.YYYY, HH:mm:ss'),
+                        style: 'footer'
+                    }
+                ],
+                styles: {
+                    header: {
+                        fontSize: 16,
+                        color: '#0277BD',
+                        bold: true,
+                        margin: [0, 5, 0, 7]
+                    },
+                    headline: {
+                        fontSize: 12,
+                        color: '#0277BD',
+                        margin: [0, 7, 0, 2]
+                    },
+                    footer: {
+                        fontSize: 8,
+                        color: 'gray',
+                        margin: [45, 0, 0, 0]
+                    },
+                    content: {
+                        fontSize: 10,
+                        color: 'black',
+                        margin: [0, 0, 0, 2]
+                    },
+                    separator: {
+                        margin: [0, 0, 0, 8]
+                    }
                 }
-            }
-        };
+            };
 
+            events.forEach(function (e) {
+                doc.content.push({text: e.title, style: 'headline'});
 
-        events.forEach(function(e){
-            console.log(JSON.stringify(e));
-            doc.content.push({text: e.title, style: 'headline'});
+                doc.content.push({text: moment(e.startdate).format('DD.MM.YYYY, HH:mm') + ' Uhr', style: ['content']});
 
-            var org = e.organization.name + ' | ' + e.organization.street + ' | ' + e.organization.zip + ' | ' + e.organization.city;
-            doc.content.push({text: org, style: ['content', 'separator']});
+                var org = e.organization.name + ' | ' + e.organization.street + ' | ' + e.organization.zip + ' | ' + e.organization.city;
+                doc.content.push({text: org, style: ['content', 'separator']});
 
-            var helpers = e.helpers.length+' Helfer Angemeldet: ';
-            e.helpers.forEach(function(h, idx){
-                helpers += h.name;
-                if(idx < e.helpers.length-1)
-                    helpers += ', ';
+                var helpers = e.helpers.length + ' Helfer Angemeldet: ';
+                e.helpers.forEach(function (h, idx) {
+                    helpers += h.name;
+                    if (idx < e.helpers.length - 1)
+                        helpers += ', ';
+                });
+                doc.content.push({text: helpers, style: ['content', 'separator']});
             });
-            doc.content.push({text: helpers, style: ['content', 'separator']});
-        });
 
-        pdfMake.createPdf(doc).open();
-    });
+            pdfMake.createPdf(doc).open();
+        });
+    }
 
 
 
